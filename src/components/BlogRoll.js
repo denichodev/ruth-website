b15ac383b3b2b2eb5357263b154035ac9174d4fb
Deni Cho <deni.afrizal@kumparan.com>
2019-11-20 22:55:58 +0700

    Edit maxwidth

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -130,7 +130,7 @@ export default () => (
                 featuredpost
                 featuredimage {
                   childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
+                    fluid(maxWidth: 284, quality: 100) {
                       ...GatsbyImageSharpFluid
                     }
                   }
