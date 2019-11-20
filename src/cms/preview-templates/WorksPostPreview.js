35a8b2d51d18949fb133dad9d2a78028580e416c
Deni Cho <denichodev@gmail.com>
2019-11-20 20:07:44 +0700

    Update works post

---
+++ src/cms/preview-templates/WorksPostPreview.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { WorksPostTemplate } from "../../templates/works-post";
+
+const WorksPostPreview = ({ entry, widgetFor }) => (
+  <WorksPostTemplate
+    content={widgetFor("body")}
+    description={entry.getIn(["data", "description"])}
+    tags={entry.getIn(["data", "tags"])}
+    title={entry.getIn(["data", "title"])}
+  />
+);
+
+WorksPostPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  widgetFor: PropTypes.func
+};
+
+export default WorksPostPreview;
