1e1c9aade9e158ee0174dd837df85400b89bbd3d
Deni Cho <denichodev@gmail.com>
2020-01-07 20:07:24 +0800

    ESLINT

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import styles from "../templates/styles.module.css";
 import allStyles from "./all.module.css";
@@ -9,7 +8,31 @@ const Footer = class extends React.Component {
     return (
       <footer className={styles.worksContainer}>
         <div className={allStyles.footer}>
-          <p><a href="mailto:ruthhutagalungs@gmail.com">ruthhutagalungs@gmail.com</a> | <a href="#" target="_blank" rel="noopener noreferrer">Resume</a> | <a href="https://www.linkedin.com/in/ruth-hutagalung-5b7b46114/" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a href="https://www.instagram.com/080011ultra/" target="_blank" rel="noopener noreferrer">Instagram</a></p>
+          <p>
+            <a href="mailto:ruthhutagalungs@gmail.com">
+              ruthhutagalungs@gmail.com
+            </a>{" "}
+            |{" "}
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              Resume
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://www.linkedin.com/in/ruth-hutagalung-5b7b46114/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://www.instagram.com/080011ultra/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </p>
         </div>
       </footer>
     );
