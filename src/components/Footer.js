19abb11ac655aabc8f5b2ede984f8453dd0abf4d
Deni Cho <denichodev@gmail.com>
2020-01-07 19:17:33 +0800

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = class extends React.Component {
     return (
       <footer className={styles.worksContainer}>
         <div className={allStyles.footer}>
-          <p>Made with YOB</p>
+          <p><a href="mailto:ruthhutagalungs@gmail.com">ruthhutagalungs@gmail.com</a> | <a href="#" target="_blank" rel="noopener noreferrer">Resume</a> | <a href="https://www.linkedin.com/in/ruth-hutagalung-5b7b46114/" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a href="https://www.instagram.com/080011ultra/" target="_blank" rel="noopener noreferrer">Instagram</a></p>
         </div>
       </footer>
     );
